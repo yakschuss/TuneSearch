12a2191a42aef6e8d1671f1564dc6fb30a57c687
Jack Schuss <jackschuss@gmail.com>
2016-07-26 09:06:07 -0400

    stub out tests for search request

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,4 +2,29 @@ require 'rails_helper'
 
 RSpec.describe SearchController, type: :controller do
 
+  describe "GET search" do
+    it "returns the correct values when artists and albums aren't present" do
+
+      Song.create!(title: "onesong")
+      Song.create!(title: "song-one")
+      Song.create!(title: "song-three")
+
+      get :search, search_bar: {song_name: "one", artist_name: nil, album_name: nil}
+
+      expect(response).to match_array(%w(onesong song-one))
+
+    end
+
+    it "returns the correct values when songs and albums aren't present" do
+      get :search, search_bar: {song_name: nil, artist_name: "something here", album_name: nil}
+    end
+
+    it "returns the correct values when songs and artists aren't present" do
+      get :search, search_bar: {song_name: nil, artist_name: nil, album_name: "something here"}
+    end
+
+
+
+
+  end
 end
